c1db4853b7356d3c3b2a2ed0260e2ee67ae209aa
Ammar Ash shiddiq <Ammarashshiddiq2@gmail.com>
2023-09-06 17:43:48 +0700

    fix register

--- client/src/components/Register/RegisterCard.js
+++ client/src/components/Register/RegisterCard.js
@@ -2,118 +2,107 @@ import React from "react";
 
 function RegisterCard() {
   return (
-    <div class="flex w-[552px] p-[30px] md:p-[30px]-[90px] flex-col items-center gap-[20px] flex-shrink-0 rounded-2xl border border-gray-400 bg-[#71825E]">
-      <div class="h-[51px] text-[#FFF] text-center font-dm-sans text-[35px] font-normal self-stretch">
-        Create your Account
-      </div>
-      <div class="flex flex-col justify-center items-center gap-[20px]">
+    <div class="flex w-[387.107px] p-[21.849px 65.548px] flex-col items-center gap-[14.566px] flex-shrink-0">
+      <div class="w-full h-full rounded-[14.566px] border border-[0.728px] border-gray-400 bg-[#71825E]">
+        <div class="w-[383.355px] h-[36.903px] text-white text-center font-dmsans text-[25.491px] font-normal leading-normal">
+          Create your Account
+        </div>
 
-        <div class="flex flex-col items-start gap-[10px]">
-          <div class="flex flex-col items-start gap-[5px]">
-            <span class="text-white font-dm-sans text-[20px] font-bold">
-              Username
-            </span>
-            <div class="flex w-[360px] h-[50px] p-[0px] 10px flex-col justify-center items-start gap-[10px] rounded-6 border border-gray-400 bg-white">
-              <div class="w-[233px] h-[25px] flex-shrink-0 text-[#4E5A6E] font-dm-sans text-[20px] font-normal">
-                AbbyMVP
+        <div class="flex flex-col justify-center items-center gap-[14.566px]">
+          <div class="flex flex-col items-start gap-[7.283px]">
+            <div class="flex flex-col items-start gap-[3.642px]">
+              <span class="text-white font-dmsans text-[14.566px] font-bold leading-normal">
+                Email Address
+              </span>
+              <div class="flex w-[252.461px] h-[37.819px] p-[0 7.283] flex-col justify-center items-start gap-[7.283px] rounded-[4.37px border border-[0.728px] border-gray-400 bg-[#FFF]">
+                <span class="w-[163.399px] h-[18.91px] flex-shrink-0 text-[#4E5A6E] font-dmsans text-[14.566px] font-normal leading-normal">
+                  qissa@ristek.cs.ui.ac.id
+                </span>
+              </div>
+            </div>
+
+            <div class="flex flex-col items-start gap-[3.642px]">
+              <span class="text-white font-dmsans text-[14.566px] font-bold leading-normal">
+                Email Address
+              </span>
+              <div class="flex w-[252.461px] h-[37.819px] p-[0 7.283] flex-col justify-center items-start gap-[7.283px] rounded-[4.37px border border-[0.728px] border-gray-400 bg-[#FFF]">
+                <span class="w-[163.399px] h-[18.91px] flex-shrink-0 text-[#4E5A6E] font-dmsans text-[14.566px] font-normal leading-normal">
+                  qissa@ristek.cs.ui.ac.id
+                </span>
+              </div>
+            </div>
+
+            <div class="flex flex-col items-start gap-[3.642px]">
+              <span class="text-white font-dmsans text-[14.566px] font-bold leading-normal">
+                Email Address
+              </span>
+              <div class="flex w-[252.461px] h-[37.819px] p-[0 7.283] flex-col justify-center items-start gap-[7.283px] rounded-[4.37px border border-[0.728px] border-gray-400 bg-[#FFF]">
+                <span class="w-[163.399px] h-[18.91px] flex-shrink-0 text-[#4E5A6E] font-dmsans text-[14.566px] font-normal leading-normal">
+                  qissa@ristek.cs.ui.ac.id
+                </span>
+              </div>
+            </div>
+
+            <div class="flex flex-col items-start gap-[3.642px]">
+              <span class="text-white font-dmsans text-[14.566px] font-bold leading-normal">
+                Email Address
+              </span>
+              <div class="flex w-[252.461px] h-[37.819px] p-[0 7.283] flex-col justify-center items-start gap-[7.283px] rounded-[4.37px border border-[0.728px] border-gray-400 bg-[#FFF]">
+                <span class="w-[163.399px] h-[18.91px] flex-shrink-0 text-[#4E5A6E] font-dmsans text-[14.566px] font-normal leading-normal">
+                  qissa@ristek.cs.ui.ac.id
+                </span>
+              </div>
+            </div>
+
+            <div class="flex flex-col items-start gap-[3.642px]">
+              <span class="text-white font-dmsans text-[14.566px] font-bold leading-normal">
+                Email Address
+              </span>
+              <div class="flex w-[252.461px] h-[37.819px] p-[0 7.283] flex-col justify-center items-start gap-[7.283px] rounded-[4.37px border border-[0.728px] border-gray-400 bg-[#FFF]">
+                <span class="w-[163.399px] h-[18.91px] flex-shrink-0 text-[#4E5A6E] font-dmsans text-[14.566px] font-normal leading-normal">
+                  qissa@ristek.cs.ui.ac.id
+                </span>
+              </div>
+            </div>
+
+            <div class="flex flex-col items-start gap-[3.642px]">
+              <span class="text-white font-dmsans text-[14.566px] font-bold leading-normal">
+                Email Address
+              </span>
+              <div class="flex w-[252.461px] h-[37.819px] p-[0 7.283] flex-col justify-center items-start gap-[7.283px] rounded-[4.37px border border-[0.728px] border-gray-400 bg-[#FFF]">
+                <span class="w-[163.399px] h-[18.91px] flex-shrink-0 text-[#4E5A6E] font-dmsans text-[14.566px] font-normal leading-normal">
+                  qissa@ristek.cs.ui.ac.id
+                </span>
+              </div>
+            </div>
+
+            <div class="flex flex-col items-start gap-[3.642px]">
+              <span class="text-white font-dmsans text-[14.566px] font-bold leading-normal">
+                Email Address
+              </span>
+              <div class="flex w-[252.461px] h-[37.819px] p-[0 7.283] flex-col justify-center items-start gap-[7.283px] rounded-[4.37px border border-[0.728px] border-gray-400 bg-[#FFF]">
+                <span class="w-[163.399px] h-[18.91px] flex-shrink-0 text-[#4E5A6E] font-dmsans text-[14.566px] font-normal leading-normal">
+                  qissa@ristek.cs.ui.ac.id
+                </span>
               </div>
             </div>
           </div>
-        </div>
 
-        <div class="flex flex-col items-start gap-[10px]">
-          <div class="flex flex-col items-start gap-[5px]">
-            <span class="text-white font-dm-sans text-[20px] font-bold">
-              Email Address
-            </span>
-            <div class="flex w-[360px] h-[50px] p-[0px] 10px flex-col justify-center items-start gap-[10px] rounded-6 border border-gray-400 bg-white">
-              <div class="w-[233px] h-[25px] flex-shrink-0 text-[#4E5A6E] font-dm-sans text-[20px] font-normal">
-                qissa@ristek.cs.ui.ac.id
-              </div>
+          <div class="flex w-[113.014px] h-[49px] p-[8.507px] justify-center items-center rounded-[10px] bg-[#305C7D]">
+            <div class="w-[98px] h-[33px] flex flex-col justify-center flex-shrink-0 text-white text-center font-poppins text-[14px] font-semibold leading-[140%]">
+              Sign Up
             </div>
           </div>
+
+          <div class="flex flex-row w-[168px] h-[25px] justify-center text-[var(--State---Blue, #FFF)] text-center font-poppins text-[12px] font-normal leading-[160%] gap-1">
+                    <div>
+                        Have an account?
+                    </div>
+                    <div class="text-[#03B3D7]">
+                        Sign in
+                    </div>
+                </div>
         </div>
-
-        <div class="flex flex-col items-start gap-[10px]">
-          <div class="flex flex-col items-start gap-[5px]">
-            <span class="text-white font-dm-sans text-[20px] font-bold">
-              Password
-            </span>
-            <div class="flex w-[360px] h-[50px] p-[0px] 10px flex-col justify-center items-start gap-[10px] rounded-6 border border-gray-400 bg-white">
-              <div class="w-[233px] h-[25px] flex-shrink-0 text-[#4E5A6E] font-dm-sans text-[20px] font-normal">
-                Enter your password here
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <div class="flex flex-col items-start gap-[10px]">
-          <div class="flex flex-col items-start gap-[5px]">
-            <span class="text-white font-dm-sans text-[20px] font-bold">
-              Confirm Password
-            </span>
-            <div class="flex w-[360px] h-[50px] p-[0px] 10px flex-col justify-center items-start gap-[10px] rounded-6 border border-gray-400 bg-white">
-              <div class="w-[233px] h-[25px] flex-shrink-0 text-[#4E5A6E] font-dm-sans text-[20px] font-normal">
-                Enter your password again
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <div class="flex flex-col items-start gap-[10px]">
-          <div class="flex flex-col items-start gap-[5px]">
-            <span class="text-white font-dm-sans text-[20px] font-bold">
-              Nama
-            </span>
-            <div class="flex w-[360px] h-[50px] p-[0px] 10px flex-col justify-center items-start gap-[10px] rounded-6 border border-gray-400 bg-white">
-              <div class="w-[233px] h-[25px] flex-shrink-0 text-[#4E5A6E] font-dm-sans text-[20px] font-normal">
-                Abby Marvel
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <div class="flex flex-col items-start gap-[10px]">
-          <div class="flex flex-col items-start gap-[5px]">
-            <span class="text-white font-dm-sans text-[20px] font-bold">
-              Lokasi
-            </span>
-            <div class="flex w-[360px] h-[50px] p-[0px] 10px flex-col justify-center items-start gap-[10px] rounded-6 border border-gray-400 bg-white">
-              <div class="w-[233px] h-[25px] flex-shrink-0 text-[#4E5A6E] font-dm-sans text-[20px] font-normal">
-                Jakarta Dropdown
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <div class="flex flex-col items-start gap-[10px]">
-          <div class="flex flex-col items-start gap-[5px]">
-            <span class="text-white font-dm-sans text-[20px] font-bold">
-              No. Telp
-            </span>
-            <div class="flex w-[360px] h-[50px] p-[0px] 10px flex-col justify-center items-start gap-[10px] rounded-6 border border-gray-400 bg-white">
-              <div class="w-[233px] h-[25px] flex-shrink-0 text-[#4E5A6E] font-dm-sans text-[20px] font-normal">
-                08571XXXX
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <div class="flex flex-col items-start gap-[10px]">
-          <div class="flex flex-col items-start gap-[5px]">
-            <span class="text-white font-dm-sans text-[20px] font-bold">
-              Instagram
-            </span>
-            <div class="flex w-[360px] h-[50px] p-[0px] 10px flex-col justify-center items-start gap-[10px] rounded-6 border border-gray-400 bg-white">
-              <div class="w-[233px] h-[25px] flex-shrink-0 text-[#4E5A6E] font-dm-sans text-[20px] font-normal">
-                @abbymarvel
-              </div>
-            </div>
-          </div>
-        </div>
-
-        
-
       </div>
     </div>
   );
