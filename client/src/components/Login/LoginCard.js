c561d20117abdab84432959d8f6c3348d5742bd0
abbymarvel <abbymarvel97@gmail.com>
2023-09-06 08:51:45 +0700

    interactive login page

--- client/src/components/Login/LoginCard.js
+++ client/src/components/Login/LoginCard.js
@@ -1,6 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 function LoginCard() {
+    // State to hold the values of the input fields
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    // Event handler for the first input field
+    const handleEmail = (e) => {
+        setEmail(e.target.value);
+    };
+
+    // Event handler for the second input field
+    const handlePassword = (e) => {
+        setPassword(e.target.value);
+    }
+
+    const LoginButton = () => {
+        setEmail('');
+        setPassword('');
+        console.log(email, password);
+    }
+
+    const ToRegisterButton = () => {
+        console.log('register');
+    }
+
     return (
         <div class="flex flex-col justify-center items-center w-548 py-[30px] px-[90px] gap-[20px] rounded-[20px] bg-[#71825E]">
             <div class="h-[51px] text-[#FFF] text-center font-dm-sans text-[35px] font-normal self-stretch">
@@ -9,38 +33,44 @@ function LoginCard() {
             <div class="flex flex-col justify-center items-center gap-[20px]">
                 <div class="flex flex-col items-start gap-[25px]">
                     <div class="flex flex-col items-start gap-5">
-                        <div class="text-white font-dm-sans text-[20px] font-bold">
+                        <div class="text-white font-dm-sans text-[20px] font-bold w-[360px]">
                             Email Address
                         </div>
-                        <div class="flex flex-col w-[360px] h-[50px] px-[10px] justify-center items-start gap-[10px] rounded-[6px] border border-[#404040] bg-white">
-                            <div class="w-233 h-25 flex-shrink-0 text-[#4E5A6E] font-dm-sans text-[20px] font-normal">
-                                qissa@ristek.cs.ui.ac.id
-                            </div>
-                        </div>
+                        <input
+                            type="text"
+                            id="email"
+                            value={email}
+                            onChange={handleEmail}
+                            placeholder="qissa@ristek.cs.ui.ac.id"
+                            className="w-full h-[50px] px-3 text-[#4E5A6E] font-dm-sans text-[20px] font-normal rounded border border-[#404040] focus:outline-none focus:border-[#3182CE] focus:ring focus:ring-[#3182CE]"
+                        />
                     </div>
                     <div class="flex flex-col items-start gap-5">
-                        <div class="text-white font-dm-sans text-[20px] font-bold">
+                        <div class="text-white font-dm-sans text-[20px] font-bold w-[360px]">
                             Password
                         </div>
-                        <div class="flex flex-col w-[360px] h-[50px] px-[10px] justify-center items-start gap-[10px] rounded-[6px] border border-[#404040] bg-white">
-                            <div class="w-233 h-25 flex-shrink-0 text-[#4E5A6E] font-dm-sans text-[20px] font-normal">
-                                ********
-                            </div>
-                        </div>
+                        <input
+                            type="text"
+                            id="password"
+                            value={password}
+                            onChange={handlePassword}
+                            placeholder="********"
+                            className="w-full h-[50px] px-3 text-[#4E5A6E] font-dm-sans text-[20px] font-normal rounded border border-[#404040] focus:outline-none focus:border-[#3182CE] focus:ring focus:ring-[#3182CE]"
+                        />
                     </div>
                 </div>
-                <div class="flex w-[113.014px] h-[49px] p-[8.507px] justify-center items-center rounded-[10px] bg-[#305C7D]">
+                <button onClick={LoginButton} class="flex w-[113.014px] h-[49px] p-[8.507px] justify-center items-center rounded-[10px] bg-[#305C7D]">
                     <div class="w-[98px] h-[33px] flex flex-col justify-center flex-shrink-0 text-white text-center font-poppins text-[14px] font-semibold leading-[140%]">
                         LOGIN
                     </div>
-                </div>
-                <div class="flex flex-row w-[168px] h-[25px] justify-center text-center font-poppins text-[12px] font-normal leading-[160%] gap-1">
+                </button>
+                <div class="flex flex-row w-[168px] h-[25px] items-center text-center justify-center font-poppins text-[12px] font-normal leading-[160%] gap-1">
                     <div className="text-[#FFFFFF]">
                         New to Gatherly?
                     </div>
-                    <div class="text-[#03B3D7]">
+                    <button onClick={ToRegisterButton} class="text-[#03B3D7]">
                         Register
-                    </div>
+                    </button>
                 </div>
             </div>
         </div>
