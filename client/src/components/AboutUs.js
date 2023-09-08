12dd3ba6b42eefde0daa9309b611ca2570141598
Syarifah <syarifah@MacBook-Air-Syarifah.local>
2023-09-08 16:26:54 +0700

    fixing AboutUs page

--- client/src/components/AboutUs.js
+++ client/src/components/AboutUs.js
@@ -1,20 +1,25 @@
 import React from 'react'
 import Navbar from './UI/Commons/Navbar'
-import Introduction from './AboutUs/introduction.js'
-import Joinus from './AboutUs/joinus.js'
-import Ourmission from './AboutUs/ourmission.js'
-import Ourvalues from './AboutUs/ourvalues.js'
+import Introduction from './AboutUs/introduction'
+import OurMission from './AboutUs/ourmission'
+import JoinUs from './AboutUs/joinus'
+import Ourvalues from './AboutUs/ourvalues'
 
-function CreateProgram() {
+function AboutUs() {
     return (
-        <div class="flex justify-start items-center">
-            <Navbar />
-            <Introduction />
-            <Joinus />
-            <Ourmission />
-            <Ourvalues />
-        </div >
+        <div class="flex w-[1440px] h-[1024px] items-start">
+            <div class="flex flex-col w-[333px] h-[1024px] py-[30px] px-0 justify-between items-center flex-shrink-0">
+                <Navbar />
+            </div>
+            <div class="flex flex-col w-[1107px] p-[80px] flex-shrink-0 items-center gap-[75px]">
+                <Introduction/>
+                <OurMission/>
+                <JoinUs/>
+                <Ourvalues/>
+            </div>
+        </div>
+
     )
 }
 
-export default CreateProgram
+export default AboutUs
