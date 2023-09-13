22253944d4c2118fe59dfab9f6b09807ca915a65
Ammar Ash Shiddiq <ammar.ash@ui.ac.id>
2023-09-13 17:36:53 +0700

    Add Attendance

---
+++ client/src/components/Attendance.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from './UI/Commons/Navbar'
+import List from './Attendance/List'
+
+function Attendance() {
+  return (
+    <div className="w-screen h-screen">
+        <Navbar/>
+        <List/>
+        </div>
+  )
+}
+
+export default Attendance
