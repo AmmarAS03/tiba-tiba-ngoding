978f7acf606df43ee40f99b944f6a40e94cb05d4
AmmarAS03 <ammar.ash@ui.ac.id>
2023-09-15 01:55:12 +0700

    fix navbar

--- client/src/components/UI/Commons/Navbar.js
+++ client/src/components/UI/Commons/Navbar.js
@@ -12,58 +12,28 @@ function Navbar() {
     console.log("Activity");
   };
 
-
-    //TODO: cek
-    const SignOutButton = async(e) => {
-        const response = await fetch("http://localhost:5000/users/logout");
-        console.log(response);
-    }
-
-    return (
-        <div class="fixed flex flex-col h-screen py-[22.5px] w-[249.75px] justify-between items-center shrink-0 self-stretch border-r-[1.125px] border-[#71825E] bg-[#FFFFFF]">
-            <div class="flex flex-col items-center gap-[37.5px]">
-                <img src="assets/Logo.svg" alt="Your Image" class="w-[83.827px] h-[70.542px]" />
-                <div class="w-[196.501px] h-[0.75px] bg-[#10436A]"></div>
-                <button onClick={() => navigate("/dashboard")} class="w-[172.5px] h-[56.25px] hover:rounded-[7px] hover:p-[10px] hover:bg-[#F5F3F3] hover:scale-110 duration-150 flex justify-center items-center gap-[7.5px] rounded-[5.25px]">
-                    <div class="text-[#71825E] font-dm-sans text-[26.25px] font-bold leading-normal">
-                        Dashboard
-                    </div>
-                </button>
-                <button onClick={() => navigate("/product")} class="w-[172.5px] h-[56.25px] hover:rounded-[7px] hover:p-[10px] hover:bg-[#F5F3F3] hover:scale-110 duration-150 flex justify-center items-center gap-[7.5px] rounded-[5.25px]">
-                    <div  class="text-[#71825E] font-dm-sans text-[26.25px] font-bold leading-normal">
-                        Activity
-                    </div>
-                </button>
-                <button onClick={() => navigate("/leaderboard")} class="w-[172.5px] h-[56.25px] hover:rounded-[7px] hover:p-[10px] hover:bg-[#F5F3F3] hover:scale-110 duration-150 flex justify-center items-center gap-[7.5px] rounded-[5.25px]">
-                    <div class="text-[#71825E] font-dm-sans text-[26.25px] font-bold leading-normal">
-                        Leaderboard
-                    </div>
-                </button>
-                <button onClick={() => navigate("/aboutus")} class="w-[172.5px] h-[56.25px] hover:rounded-[7px] hover:p-[10px] hover:bg-[#F5F3F3] hover:scale-110 duration-150 flex justify-center items-center gap-[7.5px] rounded-[5.25px]">
-                    <div class="text-[#71825E] font-dm-sans text-[26.25px] font-bold leading-normal">
-                        About Us
-                    </div>
-                </button>
-            </div>
-            <button onClick={() => navigate("/login")} class="w-[120px] h-[39px] hover:text-[#F65C51] hover:scale-110 hover:font-medium text-[#71825E] font-normal duration-150 flex justify-center items-center gap-[7.5px] flex-shrink-0">
-                <div class="text-center font-dm-sans text-[30px] leading-normal">
-                    Sign Out
-                </div>
-            </button>
-
+  //TODO: cek
+  const SignOutButton = async (e) => {
+    const response = await fetch("http://localhost:5000/users/logout");
+    console.log(response);
+  };
 
   return (
     <div class="fixed flex flex-col h-screen py-[22.5px] w-[249.75px] justify-between items-center shrink-0 self-stretch border-r-[1.125px] border-[#71825E] bg-[#FFFFFF]">
       <div class="flex flex-col items-center gap-[37.5px]">
-        <div onClick={() => navigate("/dashboard")} style={{ cursor: "pointer" }}>
-          <img
-            src="assets/Logo.svg"
-            alt="Your Image"
-            className="w-[83.827px] h-[70.542px]"
-          />
+        <div
+          onClick={() => navigate("/dashboard")}
+          style={{ cursor: "pointer" }}
+        >
+          <div className="w-[83.827px] h-[70.542px]">
+            <img
+              src="assets/Logo.svg"
+              alt="Your Image"
+              className="w-full h-full"
+            />
+          </div>
         </div>
         <div class="w-[196.501px] h-[0.75px] bg-[#10436A]"></div>
-        
         <button
           onClick={() => navigate("/product")}
           class="w-[172.5px] h-[56.25px] hover:rounded-[7px] hover:p-[10px] hover:bg-[#F5F3F3] hover:scale-110 duration-150 flex justify-center items-center gap-[7.5px] rounded-[5.25px]"
