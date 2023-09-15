0dbde3b9f74e4f34519d667398e174e9b991cca2
abbymarvel <abbymarvel97@gmail.com>
2023-09-16 06:01:46 +0700

    Refactor: Interactive - Dashboard

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -5,11 +5,11 @@ import ListofProgramJoined from './Dashboard/ListofProgramJoined'
 
 function Dashboard() {
     return (
-        <div class="flex flex-col pl-[249.75px] w-screen h-[1024px] pt-[25px] items-center gap-[5px]">
+        <div class="flex flex-col w-screen h-[1024px] px-4 pt-[91.41px] items-center gap-[5px]">
             <Profile />
-            <div class="w-[1120px] h-[1.25px] bg-[#71825E] flex-shrink-0"></div>
+            <div class="w-full px-[20px] lg:w-[1120px] h-[1.25px] bg-[#71825E] flex-shrink-0"></div>
             <ListofProgramHeld />
-            <div class="w-[1120px] h-[1.25px] bg-[#71825E] mt-[50px] flex-shrink-0"></div>
+            <div class="w-full px-[20px] lg:w-[1120px] h-[1.25px] bg-[#71825E] mt-[50px] flex-shrink-0"></div>
             <ListofProgramJoined />
         </div>
     )
--- client/src/components/Dashboard/Profile.js
+++ client/src/components/Dashboard/Profile.js
@@ -12,7 +12,7 @@ const Profile = () => {
   //get profile
   const getUser = async() => {
     try {
-        const data = await fetch("http://localhost:5000/users", {
+        const data = await fetch("http://localhost:5371/users", {
             headers: {
                 "Authorization": `Bearer ${localStorage.getItem('token')}`
             }
@@ -37,9 +37,9 @@ const Profile = () => {
 
 
     return (
-        <div class="w-[1190px] h-[269px] py-[30px] px-[110px] justify-between flex flex-col items-center flex-shrink-0">
-            <div class="w-[962.375px] flex justify-between items-center">
-                <div class="flex justify-center items-center gap-[3.75px]">
+        <div class="w-full lg:w-[1190px] py-[30px] px-[110px] flex flex-col items-center flex-shrink-0 lg:gap-4 md:gap-2">
+            <div class="w-full flex flex-col lg:flex-row justify-between items-center gap-4">
+                <div class="flex justify-center items-center lg:gap-0 gap-4">
                     <div class="w-[109.125px] flex flex-col justify-center items-center gap-3.75">
                         <img src="assets/User.png" alt="Your Image" class="w-[67.5px] h-[67.5px]" />
                     </div>
@@ -60,7 +60,7 @@ const Profile = () => {
                         </div>
                     </div>
                 </div>
-                <div class="bg-[#71825E] w-[1.5px] h-[108.75px]" />
+                <div class="hidden lg:flexbg-[#71825E] w-[1.5px] h-[108.75px]" />
                 <div class="flex justify-center items-start gap-[22.5px]">
                     <div class="w-[225px] h-[90px] p-[15.75px 24.75px] flex justify-center items-center gap-[33.75px] rounded-[18px] border-[0.225px] border-solid border-black bg-white shadow-[4.5px_4.5px_2.5px_0px_rgba(0,0,0,0.5)]">
                         <img src="assets/Points.svg" alt="Your Image" class="w-[39.995px] h-[39.951px]" />
@@ -86,7 +86,7 @@ const Profile = () => {
                     </div>
                 </div>
             </div>
-            <div class="flex flex-col h-[50px] justify-between items-end flex-shrink-0 self-stretch">
+            <div class="flex flex-col justify-between items-end flex-shrink-0 self-stretch">
                 <button onClick={() => navigate("/createprogram")} class="rounded-[10px] bg-[#71825E] w-[350px] h-[60px] py-[8px] px-[22px] flex justify-center items-center gap-[10px] flex-shrink-0">
                     <div class="flex flex-col justify-center flex-1 self-stretch text-white text-center font-poppins font-semibold text-lg leading-[33.6px]">
                         Buat Kegiatan Baru
