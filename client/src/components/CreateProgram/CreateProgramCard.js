f35ffbe125edac1aaccef75004af15e09eb36a0e
abbymarvel <abbymarvel97@gmail.com>
2023-09-16 04:23:29 +0700

    Fix: Create Program

--- client/src/components/CreateProgram/CreateProgramCard.js
+++ client/src/components/CreateProgram/CreateProgramCard.js
@@ -7,16 +7,16 @@ const CreateProgramCard = () => {
     const [tgl, setTgl] = useState('');
     const [target, setTarget] = useState('');
 
-    
 
-     // TODO: Integrate this function with the backend (DONE)
-    const submitButton = async(e) => {
+
+    // TODO: Integrate this function with the backend (DONE)
+    const submitButton = async (e) => {
         try {
             const body = { title, deskripsi, lokasi, tgl, target }
             const response = await fetch("http://localhost:5000/programs/add-program", {
-                method: "POST", 
-                headers: { "Content-Type": "application/json" }, 
-                body: JSON.stringify(body) 
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify(body)
             });
             console.log(response);
         } catch (error) {
@@ -27,7 +27,7 @@ const CreateProgramCard = () => {
 
     return (
         <div class="flex w-full mt-[82.5px] p-[40px] flex-col justify-center items-center flex-1 self-stretch">
-            <div class="flex w-full p-[15px] flex-col justify-between items-center rounded-[15px] bg-[#71825E]">
+            <div class="flex w-full md:w-[700px] p-[15px] flex-col justify-between items-center rounded-[15px] bg-[#71825E]">
                 <div class="flex h-[60px] px-[18.75px] justify-between items-center flex-shrink-0 self-stretch">
                     <img src="assets/Arrow.svg" class="w-[38.981px] h-[30px]" alt="Your Image" />
                     <div class="flex w-full h-[38.25px] flex-col justify-center text-white text-center font-poppins font-bold text-[30px]">
@@ -35,11 +35,7 @@ const CreateProgramCard = () => {
                     </div>
                     <img src="assets/Arrow.svg" class="w-[38.981px] h-[30px] opacity-0" alt="Your Image" />
                 </div>
-                <div class="flex flex-col md:flex-row px-[60px] gap-[10px] justify-between items-center self-stretch">
-                    <div class="flex flex-col justify-center items-center gap-[15px] flex-1 flex-shrink-0 self-stretch">
-                        <span class="text-white font-poppins text-[15px] font-bold leading-[21px]">Masukan Gambar</span>
-                        <div class="flex flex-col justify-center items-center w-[225px] h-[300px] gap-[15px] rounded-[3.75px] bg-white"></div>
-                    </div>
+                <div class="flex flex-col md:flex-row px-[60px] gap-[15px] justify-between items-center self-stretch">
                     <div class="flex flex-col justify-center items-center gap-[15px] flex-1 flex-shrink-0 self-stretch">
                         <div class="flex flex-col items-start gap-[15px]">
                             <div class="flex flex-col items-start gap-[3.75px]">
@@ -50,17 +46,6 @@ const CreateProgramCard = () => {
                                     value={title}
                                     onChange={e => setTitle(e.target.value)}
                                     placeholder="Kegiatan"
-                                    className="flex flex-col items-start w-[270px] h-[37.5px] px-3 flex-shrink-0 gap-[7.5px] rounded-[4.5px] border-[0.75px] border-gray-400 bg-[#FFF]"
-                                />
-                            </div>
-                            <div class="flex flex-col items-start gap-[3.75px]">
-                                <span class="text-white font-dm-sans text-[15px] font-semibold leading-normal">Deskripsi</span>
-                                <input
-                                    type="text"
-                                    id="description"
-                                    value={deskripsi}
-                                    onChange={e => setDeskripsi(e.target.value)}
-                                    placeholder="Activity's description"
                                     className="flex flex-col items-start w-[270px] h-[37.5px] px-3 flex-shrink-0 gap-[7.5px] rounded-[4.5px] border-[0.75px] border-gray-400 bg-[#FFF]"
                                 />
                             </div>
@@ -120,11 +105,36 @@ const CreateProgramCard = () => {
                                 />
                             </div> */}
                         </div>
-                        <button onClick={submitButton}class="flex w-[113.014px] h-[49px] p-[8.507px] justify-center items-center rounded-[10px] bg-[#305C7D]">
+                        <button onClick={submitButton} class="flex w-[113.014px] h-[49px] p-[8.507px] justify-center items-center rounded-[10px] bg-[#305C7D]">
                             <div class="w-[98px] h-[33px] flex flex-col justify-center flex-shrink-0 text-white text-center font-poppins text-[14px] font-semibold leading-[140%]">
                                 Submit
                             </div>
                         </button>
+                    </div>
+                    <div class="flex flex-col h-full justify-start items-center gap-[15px] flex-1 flex-shrink-0 self-stretch">
+                        <div class="flex flex-col items-start gap-[3.75px]">
+                            <span class="text-white font-dm-sans text-[15px] font-semibold leading-normal">Deskripsi</span>
+                            <textarea
+                                type="text"
+                                id="description"
+                                value={deskripsi}
+                                onChange={e => setDeskripsi(e.target.value)}
+                                placeholder="Activity's description"
+                                className="flex flex-col text-wrap items-start w-[270px] h-[150px] resize-none px-[3px] flex-shrink-0 gap-[7.5px] rounded-[4.5px] border-[0.75px] border-gray-400 bg-[#FFF]"
+                            />
+                        </div>
+                        <div class="flex flex-col items-start gap-[3.75px]">
+                            <span class="text-white font-dm-sans text-[15px] font-semibold leading-normal">Photo</span>
+                            <input
+                                type="file"
+                                id="description"
+                                accept=".png, .jpg, .jpeg"
+                                value={''}
+                                onChange={''}
+                                placeholder="Activity's description"
+                                className="flex flex-col text-wrap items-start w-[270px] text-white font-semibold justify-between flex-shrink-0 gap-[7.5px] rounded-[4.5px]"
+                            />
+                        </div>
                     </div>
                 </div>
 
