9b4376778085c5566608d56b37769db668894f4a
AmmarAS03 <ammar.ash@ui.ac.id>
2023-09-15 13:44:39 +0700

    responsive register

--- client/src/components/Login/Heading.js
+++ client/src/components/Login/Heading.js
@@ -4,7 +4,7 @@ function Heading() {
     return (
         <div class="flex h-[75px] items-center self-stretch gap-[15px] flex-col md:flex-row">
             <img src="assets/Logo.svg" class="w-[80px] h-[62px]" alt="Your Image" />
-            <div class="text-[#71825E] font-poppins text-[48px] font-bold leading-[140%] w-[472px] items-center " style={{ marginBottom: '16px', textAlign: 'center' }}>
+            <div class="text-[#71825E] font-poppins text-[30px] md:text-[48px] font-bold leading-[140%] w-[472px] items-center " style={{ marginBottom: '16px', textAlign: 'center' }}>
                 Gotong Ruang
             </div>
         </div>
--- client/src/components/Register/Logo.js
+++ client/src/components/Register/Logo.js
@@ -2,16 +2,16 @@ import React from "react";
 
 function Logo() {
   return (
-    <div class="flex flex-col items-center gap-[4.552px]">
+    <div class="flex flex-col items-center pt-[230px] sm:pt-0">
         {/* <img
           src="assets/Logo.svg"
           class="w-[111.77px] h-[94.056px]"
           alt="Your Image"
         /> */}
-        <img src="assets/Logo.svg" alt="Your Image Description" class="w-[210.384px] h-[190.952px]"/>
+        <img src="assets/Logo.svg" alt="Your Image Description" class=" w-[130px] h-[100px] justify-center sm:w-[210.384px] h-[190.952px]"/>
 
 
-      <span class="text-[#71825E] font-poppins text-[46.612px] font-semibold leading-[140%]">
+      <span class="text-[#71825E] justify-center font-poppins text-[34px] sm:text-[46.612px] font-bold leading-[140%]" style={{ textAlign: 'center' }}>
         Gotong Ruang
       </span>
 
